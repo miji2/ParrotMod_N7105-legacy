c341aedf37c0229426c7baba67440b943eeaac07
miji2 <miji22@gmail.com>
2016-05-25 08:37:02 -0400

    Update mkmod.sh

--- mkmod.sh
+++ mkmod.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 branch=$(git rev-parse --abbrev-ref HEAD)
 mkdir -p "ParrotMod/$branch"
-mv ParrotMod_Grouper_${branch}_2*.zip $branch/ 2>/dev/null
+mv ParrotMod_N7105_${branch}_2*.zip $branch/ 2>/dev/null
 date=$(date +%Y-%m-%d_%H.%M.%S)
 cd "$(dirname "$0")/zip"
-rm -rf ../ParrotMod_Grouper_${branch}_${date}.zip
+rm -rf ../ParrotMod_N7105_${branch}_${date}.zip
 find . -name '.DS_Store' -delete
-zip -9 -r -q ../ParrotMod_Grouper_${branch}_${date}.zip * 
-du -h ../ParrotMod_Grouper_${branch}_${date}.zip
+zip -9 -r -q ../ParrotMod_N7105_${branch}_${date}.zip * 
+du -h ../ParrotMod_N7105_${branch}_${date}.zip
 cd ..
-mv ParrotMod_Grouper_${branch}_2*.zip ParrotMod/$branch/ 2>/dev/null
+mv ParrotMod_N7105_${branch}_2*.zip ParrotMod/$branch/ 2>/dev/null
 cd ParrotMod/$branch
 cn=1
-for i in `echo ParrotMod_Grouper_${branch}_2*.zip | tr ' ' '\n' | sort -r`; do 
+for i in `echo ParrotMod_N7105_${branch}_2*.zip | tr ' ' '\n' | sort -r`; do 
 	[ "$cn" -gt 4 ] && rm "$i"
 	cn=$(($cn+1))
 done
